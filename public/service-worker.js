378f375405cd86423cf93ae4dc3cc65b3d491040
sennin361 <pokeopoke9@gmail.com>
2025-05-31 06:50:45 +0900

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'sennin-chat-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/chat.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/socket.io/socket.io.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
